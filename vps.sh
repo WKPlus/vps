df08069d6fc7b5b3bedafa22b030d532c97063b0
zhaoqifa <qifa.zhao@gmail.com>
2013-11-07 17:55:28 +0800

    add script for init and back up wordpress vps

---
+++ vps.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+WEB_ROOT_DIR="/var/www/html"
+
+function pull_specified_file
+{
+    if [ $# -lt 3 ];then
+       return 1
+    fi 
+    repo=$1
+    path=$2
+    file=$3
+    #git archive --remote=git@code.dianpingoa.com:auto/octopus.git HEAD:utils/ deploy_code.sh |tar -x
+    git archive --remote=$1 HEAD:$2 $3 |tar -x
+}
+
+function svn_export
+{
+    if [ $# -lt 1 ];then
+        return 1
+    fi
+    file=$1
+    svn export https://github.com/zhaoqifa/vps.git/trunk/$file
+}
+
+function install_tools
+{
+    yum install vim
+    yum install mysql-server
+    yum install php
+    yum install httpd
+}
+
+function init_httpd
+{
+}
+
+function update_git()
+{
+    git clone git@github.com:zhaoqifa/vps.git
+    pushd .
+    cd ${WEB_ROOT_DIR} && mysqldump -uwordpress -pwordpress wordpress > wordpress.sql && tar -zcvf blog.tgz blog/ index.php wordpress.sql
+    popd
+    mv ${WEB_ROOT_DIR}/blog.tgz vps/data/
+    cd vps && git add data/blog.tgz && git commit -m "update blog data" && git push
+}
+
+function init_vps
+{
+    install_tools
+
+    git clone https://github.com/zhaoqifa/vps.git
+    if [ $? -ne 0 ];then
+        return 1
+    fi
+    cp vps/vim/vimrc ~/.vimrc
+    cp blog.tgz ${WEB_ROOT_DIR} && cd ${WEB_ROOT_DIR} && tar -zxvf blog.tgz
+}
+
