7f62d18b94329ecc10f955dd2ef3681e832f1cac
WKPlus <qifa.zhao@gmail.com>
2017-07-12 16:45:45 +0800

    Update add_user.sql

--- add_user.sql
+++ add_user.sql
@@ -1,6 +1,6 @@
 
 
-create user 'wordpress'@'localhost' identified by 'wordpress';
+create user 'wordpress'@'localhost' identified by 'xxx';
 create database wordpress;
 grant all on wordpress.* to 'wordpress'@'localhost';
 drop user ''@'localhost';
